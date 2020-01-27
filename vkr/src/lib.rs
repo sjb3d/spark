1059ea4070806c35cf97d0e38dc88191bf605592
Simon Brown <si@sjbrown.co.uk>
2020-01-27 21:36:41 +0000

    Update to 1.2.132 Vulkan spec

--- vkr/src/lib.rs
+++ vkr/src/lib.rs
@@ -1,4 +1,4 @@
-//! Generated from vk.xml with `VK_HEADER_VERSION` 131
+//! Generated from vk.xml with `VK_HEADER_VERSION` 132
 #![allow(clippy::too_many_arguments, clippy::trivially_copy_pass_by_ref)]
 
 pub mod builder;
