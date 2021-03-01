203d77945be265c1e771e2cc911e7e029349f731
Simon Brown <si@sjbrown.co.uk>
2021-03-01 20:02:03 +0000

    Update to 1.2.171 Vulkan spec

--- spark/src/lib.rs
+++ spark/src/lib.rs
@@ -1,4 +1,4 @@
-//! Generated from vk.xml with `VK_HEADER_VERSION` 170
+//! Generated from vk.xml with `VK_HEADER_VERSION` 171
 #![allow(
     clippy::too_many_arguments,
     clippy::trivially_copy_pass_by_ref,
